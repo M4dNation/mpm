4c11d4efa756ab079d8536eb96974019a40a2eae
axelvaindal <pro.axelvaindal@gmail.com>
2016-07-16 14:14:01 +0000

    MPM V1.0.0

--- contrib/mpm.sh
+++ contrib/mpm.sh
@@ -1,1 +1,68 @@
 #!/bin/bash
+# mpm.sh
+#
+# Meteor Project Manager is a script utility to help during Meteor Application 
+# Development.
+#
+#################################################################################
+
+source mpm/contrib/.mpmconfig/.mtrconfig.cfg
+source mpm/.mpm/.functions.cfg
+source mpm/.mpm/.actions.cfg
+
+LOOP=1
+while [ $LOOP -gt 0 ]; do
+	if [ $# -eq 1 ]
+  	then
+    	ACTION="$1"
+    	LOOP=0
+	else
+		echo -e "$COLOR_INFO"
+		echo -e "Welcome in the Meteor Project Manager ! What do you want to do ? $COLOR_NORMAL"
+		read ACTION
+	fi
+	if isActionCreate $ACTION
+	then
+		create
+	fi
+	if isActionRun $ACTION
+	then
+		run
+	fi
+	if isActionReset $ACTION
+	then
+		reset
+	fi
+	if isActionUpdate $ACTION
+	then
+		update
+	fi
+	if isActionList $ACTION
+	then
+		list
+	fi
+	if isActionLogin $ACTION
+	then
+		login
+	fi
+	if isActionLogout $ACTION
+	then
+		logout
+	fi
+	if isActionPackage $ACTION
+	then
+		package
+	fi
+	if isActionPlatform $ACTION
+	then
+		platform
+	fi
+	if isActionClear $ACTION
+	then
+		clear
+	fi
+	if isActionExit $ACTION
+	then
+		LOOP=0
+	fi
+done
